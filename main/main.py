708c6de83186ba41d3bb3ca597ea22c13b55a19e
make Zhang <makeztc@gmail.com>
2018-07-17 21:08:39 +0800

    add remove_stop_word method

--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from tokenize import tokenize
+from tokenize import remove_stop_word
 
 
 if __name__ == "__main__":
@@ -9,9 +10,10 @@ if __name__ == "__main__":
                         help="Input the name of the file that you want to operate.\n"
                              "IT MUST IN THE FILE OF FOLDER NAME DATA!!!")
     parser.add_argument("--method", dest="method", type=int, default=0,
-                        help="Input one number to do something"
-                             "0: quit"
-                             "1: tokenize one file")
+                        help="Input one number to do something\n"
+                             "0: quit\n"
+                             "1: tokenize one file\n"
+                             "2: remove stop word\n")
     args = parser.parse_args()
     data_path = os.path.abspath('.') + '/data/'
     if args.method == 0:
@@ -25,3 +27,12 @@ if __name__ == "__main__":
             with open(input_file_name, "r") as fin:
                 with open(output_file_name, "w") as fout:
                     tokenize(fin, fout)
+    elif args.method == 2:
+        if args.train_filename == "":
+            pass
+        else:
+            input_file_name = data_path+args.train_filename
+            output_file_name = data_path+args.train_filename.split(".")[0]+"_remove_stop_word.data"
+            with open(input_file_name, "r") as fin:
+                with open(output_file_name, "w") as fout:
+                    remove_stop_word(fin, fout)
--- main/tokenize.py
+++ main/tokenize.py
@@ -1,4 +1,5 @@
 import jieba
+import jieba.posseg as pseg
 
 
 def tokenize(fin_, fout_):
@@ -16,3 +17,29 @@ def tokenize(fin_, fout_):
         res.append("#".join(content))
         res.append(msg[2])
         fout_.write("\t".join(res) + '\n')
+
+
+def remove_stop_word(fin_, fout_):
+    left = ['x', 'zg', 'uj', 'ul', 'e', 'd', 'uz', 'y']
+    passage = fin_.read().split('\n')
+    del (passage[-1])
+    i = 1
+    for line in passage:
+        print(i)
+        i += 1
+        msg = line.split('\t')
+        res = []
+        su = []
+        content = pseg.cut(msg[0])
+        for word, flag in content:
+            if flag not in left:
+                res.append(word)
+        su.append(' '.join(res))
+        res = []
+        content = pseg.cut(msg[1])
+        for word, flag in content:
+            if flag not in left:
+                res.append(word)
+        su.append(' '.join(res))
+        su.append(msg[2])
+        fout_.write('\t'.join(su)+'\n')
